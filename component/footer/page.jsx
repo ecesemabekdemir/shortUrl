ba794d29f91abdbd84094b49c12e6a99e50fab92
Sema Bekdemir <ecesemadimen96@gmail.com>
2024-10-11 13:04:28 +0300

    footer page olusturdum css yapılcak henüz imprt edilmedi

---
+++ component/footer/page.jsx
@@ -0,0 +1,33 @@
+export default function FooterPage() {
+  return (
+    <>
+      <div className="footerContainer">
+        <div className="logo"></div>
+        <div className="sections">
+          <div className="features">
+            <p>Link Shortening</p>
+            <p>Branded Links</p>
+            <p>Analytics</p>
+          </div>
+          <div className="resources">
+            <p>Blog</p>
+            <p>Developers</p>
+            <p>Support</p>
+          </div>
+          <div className="company">
+            <p>About</p>
+            <p>Our Team</p>
+            <p>Careers</p>
+            <p>Contact</p>
+          </div>
+        </div>
+        <div className="socialLinks">
+          <p>facebook</p>
+          <p>twitter</p>
+          <p>pinterest</p>
+          <p>instagram</p>
+        </div>
+      </div>
+    </>
+  );
+}
